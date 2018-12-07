b2f18f340a152e9b5b81c1ed91bcc7e8e7227bb1
Anthony CASANAVE <anthony.casanave@gmail.com>
2018-12-07 12:10:03 +0100

    Working index with data from external json

--- store_temp.php
+++ store_temp.php
@@ -1,20 +1,24 @@
 <?php
 
-$temperature = $_POST["temperature"];
-$humidite = $_POST["humidite"];
-
-$temperature_file = "data.json";
-$data_entry = '{"temperature":'.$temperature.', "humidite":'.$humidite.'}';
-
-echo "<span>La température est de : ".$temperature."</span>";
+$data_json = file_get_contents("php://input");
+$data_file = "data.json";
 
 
-if(isset($temperature))
-{
-    $file = fopen($temperature_file,"r+");
-    fwrite($file,$data_entry);
-    fclose($file); 
-    print_r(error_get_last());
+$data = json_decode($data_json);
+
+if(! $data){
+    http_response_code(415);
+    exit();
 }
+elseif (! $data->temperature || ! $data->humidite){
+    http_response_code(400);
+    exit();
+}
+
+    $op = file_put_contents($data_file, $data_json);
+    if(! $op){
+    http_response_code(500);
+}
+
 
 ?>
