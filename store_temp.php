4ac0b41dcef66022bfdb5c28713d6a6fd6fbca3a
Anthony CASANAVE <anthony.casanave@gmail.com>
2018-12-07 10:04:53 +0100

    Can write temperature in a txt file

--- store_temp.php
+++ store_temp.php
@@ -1,3 +1,16 @@
 <?php
 echo "<span>La température est de : ".$_POST['temperature']."</span>";
 ?>
+
+
+<?php
+if(isset($_POST['temperature']))
+{
+    $file = fopen("temperature.txt","r+");
+    fwrite($file,$_POST['temperature']);
+    fclose($file); 
+    print_r(error_get_last());
+}
+
+
+?>
